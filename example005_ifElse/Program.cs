4536cee742ad3981287ad36818f2afbcb61d6c2d
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-02-28 02:18:20 +0300

    add example 005 if-else

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Имя, сестра, мне нужно имя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "букенгем")
+{
+    Console.WriteLine("Афуавкгукух");
+}
+else
+{
+    Console.WriteLine("Пон, ");
+    Console.WriteLine(username);
+}
