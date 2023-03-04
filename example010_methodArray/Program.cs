02e749f45621155860ee98d0742def718873bcb3
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-04 04:48:01 +0300

    add Example 010

--- example010_methodArray/Program.cs
+++ example010_methodArray/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {15,62,74,58,6,68,1,48,59,69,98};
+
+int n = array.Length;
+int find = 1;
+int index = 0;
+
+while (index < n){
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
