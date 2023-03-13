6d4fe11a82a36f311cdf4e247f067e7cf5c74119
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-13 18:19:41 +0300

    fibonachi

---
+++ example013_3_fibonachi/Program.cs
@@ -0,0 +1,10 @@
+﻿double findFibonachi(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return findFibonachi(n - 1) + findFibonachi(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    System.Console.WriteLine($"f({i}) = {findFibonachi(i)}");
+}
