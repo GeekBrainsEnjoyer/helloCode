bd9a81603045334d4a23fcb8b248eb9bf5becb3e
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-13 17:33:25 +0300

    2D array

---
+++ example013_recursionAlgorithm/Program.cs
@@ -0,0 +1,40 @@
+﻿// string[,] table = new string[2,5];
+// table[1,2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"{table[rows, columns]} -");
+//     }
+// }
+
+void print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void fill2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+print2DArray(matrix);
+Console.WriteLine();
+fill2DArray(matrix);
+print2DArray(matrix);
+System.Console.WriteLine();
